778a915581717b5b294a6b8f716795b37536138b
keith-glennon <glennonk@tcd.ie>
2026-09-01 02:31:17 +0100

    Update publications.js

--- publications.js
+++ publications.js
@@ -48,7 +48,7 @@ String.raw`
   </p>
         
   <p style="color: #264f7a; font-weight: bold; margin-bottom: 0;">
-   Int.J.Mod.Phys.A https://doi.org/10.1142/S0217751X26501642 <br>
+    Int.J.Mod.Phys.A https://doi.org/10.1142/S0217751X26501642 <br>
     arXiv: 2607.01999 [hep-th]
   </p>
 </a>
